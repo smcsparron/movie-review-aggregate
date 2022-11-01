e9aaa7fbd50aba647fa4692a13df6af1fc58dfcb
Shawn McSparron <shawnmc_28@hotmail.com>
2022-11-01 21:08:37 +1100

    working html with stimulus/webpack before changes to make mobile friendly

---
+++ src/controllers/hello_controller.js
@@ -0,0 +1,12 @@
+// src/controllers/hello_controller.js
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "name" ]
+
+  greet() {
+    const element = this.nameTarget
+    const name = element.value
+    console.log(`Hello, ${name}!`)
+  }
+}
