aa060c914c9d6d50ae513494411da8eca6144ba0
Shawn McSparron <shawnmc_28@hotmail.com>
2022-11-17 11:58:48 +1100

    changed some styling

--- src/controllers/movie_search_controller.js
+++ src/controllers/movie_search_controller.js
@@ -37,9 +37,9 @@ export default class extends Controller {
             </div>
             <div class="col-6 movie-details">
               <ul class="search-card-movie-details-list">
-                <li>${movie.Title}</li>
-                <li>${movie.Year}</li>
-                <li>${movie.Type[0].toUpperCase() + movie.Type.substring(1)}</li>
+                <li class="movie-details-list-text">${movie.Title}</li>
+                <li class="movie-details-list-text">${movie.Year}</li>
+                <li class="movie-details-list-text">${movie.Type[0].toUpperCase() + movie.Type.substring(1)}</li>
               </ul>
             </div>
           </div>
@@ -91,73 +91,74 @@ export default class extends Controller {
 
     // Inserting movie data
     this.movieDetailsCardTarget.insertAdjacentHTML("afterbegin", `
-    <div class="row g-0">
-    <!-- Movie Poster -->
-    <div class="col-6 movie-poster">
-      <img src="${data.Poster}" class="img-fluid rounded-1 pt-3 movie-details-poster" alt="${data.Title} Poster">
+    <div class="row g-0" data-action="click->movie-search#toggleCard">
+      <!-- Movie Poster -->
+      <div class="col-6 movie-poster">
+        <img src="${data.Poster}" class="img-fluid rounded-1 pt-3 movie-details-poster" alt="${data.Title} Poster">
+      </div>
+      <div class="col-6 movie-details">
+        <ul>
+          <li><span style="font-weight:bold">Released:</span> ${data.Released}</li>
+          <li><span style="font-weight:bold">Rated:</span> ${data.Rated}</li>
+          <li><span style="font-weight:bold">Runtime:</span> ${data.Runtime}</li>
+          <li><span style="font-weight:bold">Genre:</span> ${data.Genre}</li>
+          <li><span style="font-weight:bold">Actors:</span> ${data.Actors}</li>
+          <li><span style="font-weight:bold">Director:</span> ${data.Director}</li>
+          <li><span style="font-weight:bold">Language:</span> ${data.Language}</li>
+          <li><span style="font-weight:bold">Country:</span> ${data.Country}</li>
+          <li><span style="font-weight:bold">Awards:</span> ${data.Awards}</li>
+        </ul>
+      </div>
+      <!-- Movie description -->
+      <div class="col-12 text-start px-3">
+        <div class="card-body">
+          <h5 class="card-title">${data.Title}</h5>
+          <p class="card-text">${data.Plot}</p>
+        </div>
+      </div>
     </div>
-    <div class="col-6 movie-details">
-      <ul>
-        <li><span style="font-weight:bold">Released:</span> ${data.Released}</li>
-        <li><span style="font-weight:bold">Rated:</span> ${data.Rated}</li>
-        <li><span style="font-weight:bold">Runtime:</span> ${data.Runtime}</li>
-        <li><span style="font-weight:bold">Genre:</span> ${data.Genre}</li>
-        <li><span style="font-weight:bold">Actors:</span> ${data.Actors}</li>
-        <li><span style="font-weight:bold">Director:</span> ${data.Director}</li>
-        <li><span style="font-weight:bold">Language:</span> ${data.Language}</li>
-        <li><span style="font-weight:bold">Country:</span> ${data.Country}</li>
-        <li><span style="font-weight:bold">Awards:</span> ${data.Awards}</li>
-      </ul>
-    </div>
-    <!-- Movie description -->
-    <div class="col-12 text-start px-3">
-      <div class="card-body">
-        <h5 class="card-title">${data.Title}</h5>
-        <p class="card-text">${data.Plot}</p>
-
-        <div class="row">
-          <!-- Rotten Tomatoes Rating -->
-          <div class="col d-flex flex-column align-items-center rating-box">
-            <a href="https://www.rottentomatoes.com/search?search=${data.Title} ${data.Year}" target="_blank" class="d-flex flex-column align-items-center">
-              <img src="./images/rottentomatoes_logo.png" class="img-review-thumbnail">
-              <div class="d-flex justify-content-center">
-                <div class="tomatometer d-flex align-items-center">
-                  <img src="./images/fresh.png" class="tomato">
-                  <span>${rottenTomatoRating ? rottenTomatoRating : "N/A"}</span>
+    <div class="row g-0 px-1">
+      <!-- Rotten Tomatoes Rating -->
+        <div class="col d-flex flex-column align-items-center rating-box">
+          <a href="https://www.rottentomatoes.com/search?search=${data.Title} ${data.Year}" target="_blank" class="d-flex flex-column align-items-center">
+            <img src="./images/rottentomatoes_logo.png" class="img-review-thumbnail">
+            <div class="d-flex justify-content-center">
+              <div class="tomatometer d-flex align-items-center">
+                <img src="./images/fresh.png" class="tomato">
+                <span>${rottenTomatoRating ? rottenTomatoRating : "N/A"}</span>
                 </div>
               </div>
             </a>
           </div>
 
-          <!-- IMDB rating -->
-          <div class="col d-flex flex-column align-items-center rating-box">
-            <a href="https://www.imdb.com/title/${data.imdbID}/" target="_blank" class="d-flex flex-column align-items-center">
-              <img src="./images/IMDB_logo.png" class="img-review-thumbnail">
-              <div class="d-flex justify-content-center">
-                <div class="tomatometer d-flex align-items-center">
-                  <img src="./images/star.png" class="tomato">
-                  <span>${this.ratings(data)['Internet Movie Database']}</span>
-                </div>
+        <!-- IMDB rating -->
+        <div class="col d-flex flex-column align-items-center rating-box">
+          <a href="https://www.imdb.com/title/${data.imdbID}/" target="_blank" class="d-flex flex-column align-items-center">
+            <img src="./images/IMDB_logo.png" class="img-review-thumbnail">
+            <div class="d-flex justify-content-center">
+              <div class="tomatometer d-flex align-items-center">
+                <img src="./images/star.png" class="tomato">
+                <span>${this.ratings(data)['Internet Movie Database']}</span>
               </div>
-            </a>
-          </div>
+            </div>
+          </a>
+        </div>
 
         </div>
 
-        <div class="row">
+        <div class="row g-0 px-1">
           <!-- Metacritic rating -->
           <div class="col d-flex flex-column align-items-center rating-box">
 
             <a href="https://www.metacritic.com/search/movie/${data.Title}/results" target="_blank" class="d-flex flex-column align-items-center"">
-              <img src="./images/metacritic_logo.png" class="img-review-thumbnail">
-              <div class="d-flex justify-content-center">
-                <div class="tomatometer d-flex align-items-center">
-                  <img src="./images/meta_positive.png" class="tomato">
-                  <span>${metacriticRating ? metacriticRating : "N/A"}</span>
+            <img src="./images/metacritic_logo.png" class="img-review-thumbnail">
+            <div class="d-flex justify-content-center">
+              <div class="tomatometer d-flex align-items-center">
+                <img src="./images/meta_positive.png" class="tomato">
+                <span>${metacriticRating ? metacriticRating : "N/A"}</span>
                 </div>
               </div>
             </a>
-
           </div>
 
           <!-- Average calculation -->
