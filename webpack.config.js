b31224f1069b10e1460a072e86a4ccb5303e0eb4
Shawn McSparron <65693795+smcsparron@users.noreply.github.com>
2023-02-06 22:22:08 +1100

    removing key

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,6 @@ module.exports = {
   mode: "production",
   devtool: "source-map",
 
-  plugins: [
-    new webpack.DefinePlugin({
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      API_HOST: JSON.stringify(process.env.API_HOST)
-    })
-  ],
-
   module: {
     rules: [
       {
